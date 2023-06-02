38fd5756fffb89747d4b96c0c0865b41c87fd801
htj7425 <htj7425@gmail.com>
2023-06-02 20:24:54 +0900

    [Feat] KakaoLogin.js

--- src/Screen/TextPage.js
+++ src/Screen/TextPage.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import * as Font from 'expo-font';
+import React, { useEffect, useState } from "react";
+import * as Font from "expo-font";
 import {
   View,
   StyleSheet,
@@ -14,19 +14,19 @@ import {
   Pressable,
   Touchable,
   TouchableOpacity,
-} from 'react-native';
-import callGoogleVisionAsync from '../Components/OcrFunction';
+} from "react-native";
+import callGoogleVisionAsync from "../Components/OcrFunction";
 
 function TextPage({ route, navigation }) {
   const { itemId, otherParam } = route.params;
-  const [textData, setTextData] = useState('');
+  const [textData, setTextData] = useState("");
   const onChangeText = (payload) => setTextData(payload);
   const [isReady, setIsReady] = useState(false);
 
   const loadFonts = async () => {
     await Font.loadAsync({
-      'SUITE-Light': require('../../assets/fonts/SUITE-Light.otf'),
-      'SUITE-Medium': require('../../assets/fonts/SUITE-Medium.otf'),
+      "SUITE-Light": require("../../assets/fonts/SUITE-Light.otf"),
+      "SUITE-Medium": require("../../assets/fonts/SUITE-Medium.otf"),
     });
     setIsReady(true);
   };
@@ -37,29 +37,29 @@ function TextPage({ route, navigation }) {
   }, []);
 
   const fetchData = async () => {
-    if (otherParam != '') {
-      setTextData('Loading ... ');
+    if (otherParam != "") {
+      setTextData("Loading ... ");
       const responseData = await callGoogleVisionAsync(otherParam);
       setTextData(responseData.text);
       console.log(responseData.text);
     }
   };
 
-  if (isReady && textData != '') {
+  if (isReady && textData != "") {
     return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#DCE2F0' }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#DCE2F0" }}>
         <KeyboardAvoidingView
-          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
           behavior="padding"
         >
           <TouchableOpacity
             style={{
-              justifyContent: 'center',
-              width: '100%',
+              justifyContent: "center",
+              width: "100%",
               marginBottom: 10,
               padding: 10,
               borderRadius: 10,
-              color: '#50586C',
+              color: "#50586C",
             }}
             activeOpacity={1}
             onPress={() => {
@@ -69,7 +69,7 @@ function TextPage({ route, navigation }) {
             <Text
               style={{
                 fontSize: 30,
-                fontFamily: 'SUITE-Medium',
+                fontFamily: "SUITE-Medium",
               }}
             >
               이미지로 문제를 구성했어요
@@ -77,7 +77,7 @@ function TextPage({ route, navigation }) {
             <Text
               style={{
                 fontSize: 20,
-                fontFamily: 'SUITE-Light',
+                fontFamily: "SUITE-Light",
                 marginTop: 10,
               }}
             >
@@ -86,7 +86,7 @@ function TextPage({ route, navigation }) {
             <Text
               style={{
                 fontSize: 20,
-                fontFamily: 'SUITE-Light',
+                fontFamily: "SUITE-Light",
               }}
             >
               나인에게 물어보기 버튼을 눌러주세요
@@ -96,8 +96,8 @@ function TextPage({ route, navigation }) {
             style={{
               flex: 1,
               borderRadius: 30,
-              backgroundColor: '#F0EDCC',
-              width: '96%',
+              backgroundColor: "#F0EDCC",
+              width: "96%",
               padding: 10,
               paddingTop: 25,
               marginBottom: 30,
@@ -116,9 +116,9 @@ function TextPage({ route, navigation }) {
           </View>
           <TouchableOpacity
             style={{
-              width: '100%',
-              alignItems: 'center',
-              justifyContent: 'center',
+              width: "100%",
+              alignItems: "center",
+              justifyContent: "center",
             }}
             activeOpacity={1}
             onPress={() => {
@@ -129,7 +129,7 @@ function TextPage({ route, navigation }) {
               style={({ pressed }) => [
                 styles.button,
                 {
-                  backgroundColor: pressed ? '#3f3f3f' : 'black', // 클릭 시 배경색 변경
+                  backgroundColor: pressed ? "#3f3f3f" : "black", // 클릭 시 배경색 변경
                 },
               ]}
             >
@@ -143,21 +143,21 @@ function TextPage({ route, navigation }) {
 }
 const styles = StyleSheet.create({
   button: {
-    width: '50%',
-    alignItems: 'center',
+    width: "50%",
+    alignItems: "center",
     paddingVertical: 12,
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: 'black',
+    backgroundColor: "black",
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    color: 'white',
-    fontFamily: 'SUITE-Medium',
+    color: "white",
+    fontFamily: "SUITE-Medium",
   },
 });
 
