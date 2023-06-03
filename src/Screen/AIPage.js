e96851de2abb83a5859637b21a6da99518b48e73
dduneon <dduneon@gmail.com>
2023-06-03 22:02:45 +0900

    [Feat] OPENAI GPT connection, API_KEY setting

--- src/Screen/AIPage.js
+++ src/Screen/AIPage.js
@@ -3,12 +3,37 @@ import { Text, View, SafeAreaView } from 'react-native';
 
 function AIPage({ navigation, route }) {
   const { itemId, otherParam } = route.params;
-  const [isLoaded, setLoaded] = useState(false);
-  const [receiveData, setReceiveData] = useState('');
+  const [isResponse, setisResponse] = useState(false);
+  const [responseText, setResponseText] = useState('');
   const apiKey = process.env.OPENAI_API_KEY;
 
   console.log('[AIPage.js] AIPage Loaded');
-  console.log('[AIPage.js] api key: ' + apiKey);
+
+  const handleSend = () => {
+    fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: inputText }],
+        max_tokens: 600,
+        temperature: 0.7,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const responseData = data.choices[0].message.content.trim();
+        setResponseText(responseData);
+      })
+      .catch((error) => {
+        // 에러 발생시 조치하기
+        console.error(error);
+      });
+  };
 
   return (
     <SafeAreaView
