f74192bd750b03990a3b07fd86768d9c3166b0ac
dduneon <dduneon@gmail.com>
2023-06-03 22:20:00 +0900

    [Feat] Response 받아오기 성공

--- src/Screen/AIPage.js
+++ src/Screen/AIPage.js
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Text, View, SafeAreaView } from 'react-native';
 
 function AIPage({ navigation, route }) {
   const { itemId, otherParam } = route.params;
   const [isResponse, setisResponse] = useState(false);
-  const [responseText, setResponseText] = useState('');
+  const [answer, setAnswer] = useState('');
+
   const apiKey = process.env.OPENAI_API_KEY;
+  const API_URL = 'https://api.openai.com/v1/chat/completions';
 
   console.log('[AIPage.js] AIPage Loaded');
 
@@ -18,23 +20,32 @@ function AIPage({ navigation, route }) {
       },
       body: JSON.stringify({
         model: 'gpt-3.5-turbo',
-        messages: [{ role: 'user', content: inputText }],
+        messages: [
+          {
+            role: 'user',
+            content: otherParam + '\n이 문제의 답과 해설을 알려줘',
+          },
+        ],
         max_tokens: 600,
         temperature: 0.7,
       }),
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         const responseData = data.choices[0].message.content.trim();
-        setResponseText(responseData);
+        setAnswer(responseData);
       })
       .catch((error) => {
         // 에러 발생시 조치하기
         console.error(error);
       });
   };
+  useEffect(() => {
+    handleSend();
+    console.log(answer);
+  }, []);
 
+  // 해야할점 : 데이터 token 수 조정하기
   return (
     <SafeAreaView
       style={{
