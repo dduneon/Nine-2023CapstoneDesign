effb84d78b1308310496dadab9428184b1c5e4ca
xpexpe <xpexpe3@icloud.com>
2023-06-06 06:45:56 +0900

    [Fix] AIPage에서 뒤로가기 못하도록 수정

--- App.js
+++ App.js
@@ -87,6 +87,7 @@ export default function App() {
             name="AI"
             component={AIPage}
             options={{
+              gestureEnabled: false,
               headerShown: false,
             }}
           />
