4cf78e2d767e1da67bf7d6bcded0db29ce5fc195
htj7425 <htj7425@gmail.com>
2023-10-03 15:29:15 +0900

    [DOCS] Firebase API Key Update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,14 @@ import { getDatabase, ref, onValue, set, query } from 'firebase/database';
 const apiKey = process.env.FIREBASE_API_KEY;
 
 const firebaseConfig = {
-  apiKey: apiKey,
-  authDomain: 'nine-d621e.firebaseapp.com',
-  projectId: 'nine-d621e',
-  storageBucket: 'nine-d621e.appspot.com',
-  messagingSenderId: '760683318872',
-  appId: '1:760683318872:web:38a37e47c3906e795d042d',
-  measurementId: 'G-QL3WX0DT82',
+  apiKey: 'AIzaSyBiO0oy1c7gwlThIN_YrF-6lRVqHD_s42U',
+  authDomain: 'nine-185dc.firebaseapp.com',
+  databaseURL: 'https://nine-185dc-default-rtdb.firebaseio.com',
+  projectId: 'nine-185dc',
+  storageBucket: 'nine-185dc.appspot.com',
+  messagingSenderId: '409930738037',
+  appId: '1:409930738037:web:a65c03b7e8b1603bae8b3c',
+  measurementId: 'G-ZSMF2NKM0Q',
 };
 
 initializeApp(firebaseConfig);
